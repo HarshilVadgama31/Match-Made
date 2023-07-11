fb6312d5cbaf691622c68c4f27aefd4e67f9e788
HarshilVadgama31 <vadgamaharshil99@gmail.com>
2023-07-11 23:45:23 +0530

    Landing Page Almost Done

--- match-made/tailwind.config.js
+++ match-made/tailwind.config.js
@@ -2,8 +2,24 @@
 export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bg_light:'#fff4f4',
+        font_light:'#000000',
+        card_light:'#f7e6c4',
+        button_light:'#f1c376',
+        bg_dark:'#000000',
+        font_dark:'#fff4f4',
+        card_dark:'#27374d',
+        button_dark:'#506a7e',
+      },
+      backgroundImage: {
+        'landing_dark': "url('../image/Image_Landing_Page_dark.png')",
+        'landing_light': "url('../image/Image_Landing_Page_light.png')",
+      },
+    },
   },
   plugins: [],
 }
